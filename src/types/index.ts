049f628f6307d446daa3b5db163b9eff5472a558
Anastasia Chygynova <anastasia_chygynova@mail.ru>
2025-06-23 18:07:13 +0300

    Update

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,48 +1,49 @@
 // Типы данных из API
 
 export interface Product {
-	id: string;
-	title: string;
-	description: string;
-	category: string;
-	price: number;
-	image: string;
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  image: string;
 }
 
 export interface Order {
-	id: string;
-	items: Product[];
-	total: number;
-	address: string;
-	email: string;
-	phone: string;
-	payment: 'card' | 'cash';
+  id: string;
+  items: Product[];
+  total: number;
+  address: string;
+  email: string;
+  phone: string;
+  payment: 'card' | 'cash';
 }
 
 export interface User {
-	id: string;
-	email: string;
-	phone: string;
+  id: string;
+  email: string;
+  phone: string;
 }
 
 // Интерфейсы отображения
+
 export interface ProductView {
-	id: string;
-	title: string;
-	price: string;
-	image: string;
-	category: string;
-	description?: string;
+  id: string;
+  title: string;
+  price: string;
+  image: string;
+  category: string;
+  description?: string;
 }
 
 export interface OrderView {
-	id: string;
-	items: ProductView[];
-	total: string;
-	address: string;
-	email: string;
-	phone: string;
-	payment: string;
+  id: string;
+  items: ProductView[];
+  total: string;
+  address: string;
+  email: string;
+  phone: string;
+  payment: 'card' | 'cash';
 }
 
 // Интерфейс API-клиента
@@ -50,13 +51,17 @@ export interface OrderView {
 export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
 
 export interface IApiClient {
-	get<T = unknown>(uri: string): Promise<T>;
-	post<T = unknown>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T = unknown>(uri: string): Promise<T>;
+  post<T = unknown>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods
+  ): Promise<T>;
 }
 
 export type ApiListResponse<Type> = {
-	total: number;
-	items: Type[];
+  total: number;
+  items: Type[];
 };
 
 // Интерфейсы базовых классов
@@ -65,26 +70,26 @@ export type EventName = string | RegExp;
 export type Subscriber = (...args: unknown[]) => void;
 
 export interface IEvents {
-	on<T = unknown>(event: EventName, callback: (data: T) => void): void;
-	off(event: EventName, callback: Subscriber): void;
-	emit<T = unknown>(event: string, data?: T): void;
-	onAll?(callback: (event: EmitterEvent) => void): void;
-	offAll?(): void;
-	trigger?<T = unknown>(event: string, context?: Partial<T>): (data: T) => void;
+  on<T = unknown>(event: EventName, callback: (data: T) => void): void;
+  off(event: EventName, callback: Subscriber): void;
+  emit<T = unknown>(event: string, data?: T): void;
+  onAll?(callback: (event: EmitterEvent) => void): void;
+  offAll?(): void;
+  trigger?<T = unknown>(event: string, context?: Partial<T>): (data: T) => void;
 }
 
-export interface EmitterEvent {
-	eventName: string;
-	data: unknown;
+export interface EmitterEvent<T = unknown> {
+  eventName: string;
+  data: T;
 }
 
 // Перечисления событий и их интерфейсы
 
 export enum AppEvent {
-	ProductAdded = 'product:added',
-	ProductRemoved = 'product:removed',
-	OrderCreated = 'order:created',
-	OrderFailed = 'order:failed',
+  ProductAdded = 'product:added',
+  ProductRemoved = 'product:removed',
+  OrderCreated = 'order:created',
+  OrderFailed = 'order:failed',
 }
 
 // Вспомогательные типы
